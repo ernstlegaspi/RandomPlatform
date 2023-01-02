aab8d000098db43b40ab25e3bf994351f60ee832
Akie <legaspiernstdanielle@gmail.com>
2023-01-02 15:51:51 +0800

    Add project files.

---
+++ Source/RandomPlatform/RandomPlatformGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RandomPlatformGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RANDOMPLATFORM_API ARandomPlatformGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
