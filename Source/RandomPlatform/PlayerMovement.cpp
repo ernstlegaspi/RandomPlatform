c0cde5ede33aee6ef137f1476de208ec2dcb34fe
Akie <legaspiernstdanielle@gmail.com>
2023-01-02 18:47:14 +0800

    Added Player Movement
    Player Movement using Enhanced Input System

--- Source/RandomPlatform/PlayerMovement.cpp
+++ Source/RandomPlatform/PlayerMovement.cpp
@@ -1,6 +1,7 @@
-
-
 #include "PlayerMovement.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 APlayerMovement::APlayerMovement() {
 	PrimaryActorTick.bCanEverTick = true;
@@ -8,12 +9,34 @@ APlayerMovement::APlayerMovement() {
 
 void APlayerMovement::BeginPlay() {
 	Super::BeginPlay();
+
+	if(APlayerController* PlayerController = Cast<APlayerController>(GetController())) {
+		if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+			Subsystem->AddMappingContext(PlayerMappingContext, 0);
+		}
+	}
 }
 
 void APlayerMovement::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 }
 
+void APlayerMovement::Move(const FInputActionValue& Value) {
+	FVector DirVal = Value.Get<FVector>();
+
+	if(Controller && (DirVal.X != 0 || DirVal.Y != 0)) {
+		FVector _Forward = GetActorForwardVector();
+		FVector _Sidewards = GetActorRightVector();
+
+		AddMovementInput(_Forward, DirVal.Y);
+		AddMovementInput(_Sidewards, DirVal.X);
+	}
+}
+
 void APlayerMovement::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if(UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &APlayerMovement::Move);
+	}
 }
--- Source/RandomPlatform/PlayerMovement.h
+++ Source/RandomPlatform/PlayerMovement.h
@@ -3,7 +3,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "PlayerMovement.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
 
 UCLASS()
 class RANDOMPLATFORM_API APlayerMovement : public APawn {
@@ -15,6 +19,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveInputAction;
+
+	void Move(const FInputActionValue& Value);
 public:	
 	virtual void Tick(float DeltaTime) override;
 
